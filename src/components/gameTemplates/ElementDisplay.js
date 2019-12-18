a67c1aee0df764f9f73ffd54ac9b74c6097ccc84
MrOliver243 <55758743+MrOliver243@users.noreply.github.com>
2019-12-18 10:09:19 +0000

    Update ElementDisplay.js

--- src/components/gameTemplates/ElementDisplay.js
+++ src/components/gameTemplates/ElementDisplay.js
@@ -58,6 +58,8 @@ class ElementDisplay extends Component {
     const gameImage = this.props.gameImage;
     const gameRats = this.props.gameRats;
     const gameTitle = this.props.gameTitle;
+    const gameBac = this.props.gameBac;
+    const gameVid = this.props.gameVid;
     const gameRev =
       this.props.gameReview == "none" ? "" : this.props.gameReview;
     const gameHandle =
@@ -65,62 +67,71 @@ class ElementDisplay extends Component {
     const expand = this.state.expand ? "d-flex container" : "container";
     //const fullPage = !this.state.expand ? "container" : "d-flex container";
     return (
-      <div className={"user-projects card " + expand}>
-        <img src={gameImage} className="card-img-top"></img>
-        <div className="card-body">
-          <div className="card-title">
-            <i>
-              <strong>Ratings: </strong>
-              {gameRats}
-            </i>
-          </div>
 
-          <h6 className="card-title">
-            <i>{gameTitle}</i>
-          </h6>
-          <a className="hover" onClick={this.expandBtn}>
-            <p className="card-text">{showcasePreview}</p>
-          </a>
+      
+        <div className={"user-projects card " + expand}>
+          <img src={gameImage} className="card-img-top"></img>
+          <div className="card-body">
+            <div className="card-title">
+              <i>
+                <strong>Ratings: </strong>
+                {gameRats}
+              </i>
+            </div>
 
-          <div className="make-row">
-            <Container>
-              <Row>
-                <i class="material-icons">
-                  language
-</i><Col>{gameOfc}</Col>
-<i class="material-icons">
-book
-</i> <Col>{gameWiki}</Col>
-<i class="material-icons">
-class
-</i>  <Col>{gameWpedia}</Col>
-              </Row>
-            </Container>
-
-          </div>
-
-
-          <div className="btn-group-vertical float-right">
-            <button className=" like-btn" onClick={this.likeBtn}>
-              <i className="material-icons">keyboard_arrow_up</i>
-            </button>
-            <span className="container text-center">{this.state.total}</span>
-            <button className=" dislike-btn" onClick={this.dislikeBtn}>
-              <i className="material-icons">keyboard_arrow_down</i>
-            </button>
-            <a className="hover" onClick={this.props.pClick}>
-              <i className="icon-size material-icons">description</i>
+            <h6 className="card-title">
+              <i>{gameTitle}</i>
+            </h6>
+            <a className="hover" onClick={this.expandBtn}>
+              <p className="card-text">{showcasePreview}</p>
             </a>
-          </div>
 
-          <div>
-            <div className="member-review">
-              <div className="colb">{gameHandle}</div>
-              <p className="review">{gameRev}</p>
+            <div className="make-row">
+              <Container>
+                <Row>
+                  <i class="material-icons">
+                    language
+</i><Col>{gameOfc}</Col>
+                  <i class="material-icons">
+                    book
+</i> <Col>{gameWiki}</Col>
+                  <i class="material-icons">
+                    class
+</i>  <Col>{gameWpedia}</Col>
+                </Row>
+              </Container>
+
+
+
+              <div class="resp-container make-row">
+                <iframe class="resp-iframe" src={gameVid} gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+              </div>
+
+            </div>
+
+
+            <div className="btn-group-vertical float-right">
+              <button className=" like-btn" onClick={this.likeBtn}>
+                <i className="material-icons">keyboard_arrow_up</i>
+              </button>
+              <span className="container text-center">{this.state.total}</span>
+              <button className=" dislike-btn" onClick={this.dislikeBtn}>
+                <i className="material-icons">keyboard_arrow_down</i>
+              </button>
+              <a className="hover" onClick={this.props.pClick}>
+                <i className="icon-size material-icons">description</i>
+              </a>
+            </div>
+
+            <div>
+              <div className="member-review">
+                <div className="colb">{gameHandle}</div>
+                <p className="review">{gameRev}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      
     );
   }
 }
