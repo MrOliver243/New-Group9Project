c8a0801b0a6fc81d235ca454b33870f3df88bfc8
JasonT2018 <56918091+JasonT2018@users.noreply.github.com>
2019-12-18 10:13:26 +0000

    added about

---
+++ src/components/pages/about.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div className='container-fluid' className="center">
+
+        <h1 class="display-2" classname="font-weight-bold">About</h1>
+
+        <h2 class="display-5">The Gateway was developed and designed by Group 9 and is a website designed by gamers for gamers.</h2>
+        <h2 >It's a platform that allows users to make reviews of their favourite games so others can see which games are right for them.</h2>
+      </div>
+    );
+  }
+}
+
+export default About;
